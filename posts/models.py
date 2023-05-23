a39dfbd12245212f1957de22242000b1fb760e90
begimaiSydykova <123073587+begimaiSydykova@users.noreply.github.com>
2023-05-23 14:43:51 +0600

    Hw_2

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+class Product(models.Model):
+    image = models.ImageField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.title
+
+
